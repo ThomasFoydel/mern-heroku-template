0ebbd789e9932d25e5eda3cac293c8f409d8d67a
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-26 23:58:48 -0500

    fixed port number for dev backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const cors = require('cors');
 
 // static file declaration
