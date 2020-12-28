f7f7983aefa42eac935408c9a610963dc53a4dbe
ThomasFoydel <thomasjfoydel@gmail.com>
2020-12-28 01:19:37 -0600

    static file bug, path for production mode

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   //
   app.get('*', (req, res) => {
-    res.sendfile(path.join((__dirname = 'client/build/index.html')));
+    res.sendfile(path.join(__dirname + 'client/build/index.html'));
   });
 }
 
