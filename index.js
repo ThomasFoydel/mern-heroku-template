5743506e618b788681eb12f07ed6731416a306c2
ThomasFoydel <thomasjfoydel@gmail.com>
2021-01-17 01:02:49 -0600

    mongoose connection and engines update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // put your routes here
 // routes should begin with /api/
@@ -30,6 +32,15 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
-});
+// be sure to set your MONGO_URI in a .env file in both the root folder of your project
+// and in the config variables section on Heroku, in the settings page for your app
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    app.listen(port, () => {
+      console.log(`Server is up on port ${port}!`);
+    });
+  });
