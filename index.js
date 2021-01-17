5795352123a75cf250277572ac2e2cb79922781a
ThomasFoydel <thomasjfoydel@gmail.com>
2021-01-17 00:50:42 -0600

    proxy requests from front end to server in development

--- index.js
+++ index.js
@@ -5,20 +5,27 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 
 // put your routes here
+// routes should begin with /api/
+// since the front end proxies requests to any url beginning with /api/
+// from port 3000 to port 8000 in development
+// example route:
+// app.post("/api/auth/register", (req, res) => {....})
+//
+// and in the front end:
+// axios.post("/api/auth/register", {formDate}).then(result=>{....}).catch(err=>{....})
 
 // static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
-// production mode
+
 if (process.env.NODE_ENV === 'production') {
+  // production mode
   app.use(express.static(path.join(__dirname, 'client/build')));
-  //
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + 'client/build/index.html'));
   });
 }
 
-// build mode
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
