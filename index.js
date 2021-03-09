06752c9179a319709cfd7ac36b986695cab1f451
ThomasFoydel <thomasjfoydel@gmail.com>
2021-03-08 20:27:50 -0600

    mongoose update, static file declaration shortend

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const port = process.env.PORT || 8000;
 
 // put your routes here
@@ -23,11 +25,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} else {
-  // development mode
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/public/index.html'));
   });
 }
 
